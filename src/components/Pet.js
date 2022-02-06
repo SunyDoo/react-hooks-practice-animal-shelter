72c116f3ceae060e41eceae76241c1f30ac4daff
Sunil <snilavarath@gmail.com>
2022-02-06 16:09:19 -0500

    commit tree 25ea146cf21cb8203bf5d752e6f746383fc03d38

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Pet() {
+function Pet({ pet, onAdoptPet }) {
+
+  const [isAdopted, setIsAdopted] = useState(pet.isAdopted)
+  function handleAdopt(){
+    setIsAdopted(isAdopted=>!isAdopted)
+    onAdoptPet(pet.id)
+  }
+
   return (
     <div className="card" data-testid="pet">
       <div className="content">
-        <span className="header">
-          {/*'♀' OR '♂' */}
-          PET NAME
-        </span>
+        <span className="header">{pet.name}</span>
         <div className="meta">
-          <span className="date">PET TYPE</span>
+          <span className="date">{pet.type}{pet.gender === "male" ? ' ♂' : ' ♀'}</span>
         </div>
         <div className="description">
-          <p>Age: PET AGE</p>
-          <p>Weight: PET WEIGHT</p>
+          <p>Age: {pet.age}</p>
+          <p>Weight: {pet.weight}</p>
         </div>
       </div>
-      <div className="extra content">
-        <button className="ui disabled button">Already adopted</button>
-        <button className="ui primary button">Adopt pet</button>
-      </div>
+      <div className="extra content">{
+        isAdopted? 
+          <button className="ui disabled button">Already adopted</button> :
+          <button className="ui primary button" onClick={handleAdopt}>Adopt pet</button>
+      }</div>
     </div>
   );
 }
